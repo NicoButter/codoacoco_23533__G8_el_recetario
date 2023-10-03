37b439c3bbf017e18352a140958741d5067f1493
NicoButter <nicobutter@gmail.com>
2023-10-03 17:01:27 -0300

    mas correciones

--- js/main.js
+++ js/main.js
@@ -11,3 +11,16 @@ function shrinkHeader() {
 
 // Agrega un evento de desplazamiento (scroll) para llamar a la función shrinkHeader
 window.addEventListener("scroll", shrinkHeader);
+
+// Asignar la función al evento 'submit' del formulario
+document.getElementById("formulario-contacto").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita que se recargue la página
+    mostrarMensaje(); // Llama a la función para mostrar el mensaje
+});
+
+function mostrarMensaje() {
+    // Puedes personalizar el mensaje aquí
+    alert("¡Gracias por tu mensaje! Nos pondremos en contacto con vos muy pronto.");
+}
+
+
