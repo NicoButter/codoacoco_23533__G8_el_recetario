806ffd8ac1b646ba84b74c90392275231aa9a464
NicoButter <nicobutter@gmail.com>
2023-10-24 16:00:28 -0300

    slashes

--- js/main.js
+++ js/main.js
@@ -95,9 +95,9 @@ if (entorno_desarrollo) {
         }
     
         // Construir la ruta al archivo del header
-        var rutaHeader = "pages/header.html";
+        var rutaHeader = "/pages/header.html";
         // Construir la ruta al archivo del footer
-        var rutaFooter = "pages/footer.html";
+        var rutaFooter = "/pages/footer.html";
     
         // Cargar el header
         fetch(/*relativePrefix +*/ rutaHeader)
@@ -107,34 +107,34 @@ if (entorno_desarrollo) {
     
                 // Actualizar las rutas de las imágenes en el header
                 var logoDelicias = document.querySelector(".logoDelicias");
-                logoDelicias.src = /*relativePrefix +*/ "images/logo_recetas_deliciosas.png";
+                logoDelicias.src = /*relativePrefix +*/ "/images/logo_recetas_deliciosas.png";
     
                 var enlaceLogoDelicias = document.querySelector(".enlace_logo_delicias");
-                enlaceLogoDelicias.href = /*relativePrefix +*/ "index.html";
+                enlaceLogoDelicias.href = /*relativePrefix +*/ "/index.html";
                           
                 var fotoNicolas = document.querySelector(".foto_nicolas");
-                fotoNicolas.src = /*relativePrefix +*/ "images/foto_nicolas.jpg";
+                fotoNicolas.src = /*relativePrefix +*/ "/images/foto_nicolas.jpg";
     
                 var enlaceNicolas = document.querySelector(".enlace_nicolas");
-                enlaceNicolas.href = /*relativePrefix +*/ "pages/nicorecetas.html";
+                enlaceNicolas.href = /*relativePrefix +*/ "/pages/nicorecetas.html";
     
                 var fotoSabrina = document.querySelector(".foto_sabrina");
-                fotoSabrina.src = /*relativePrefix +*/ "images/foto_sabrina.jpg";
+                fotoSabrina.src = /*relativePrefix +*/ "/images/foto_sabrina.jpg";
     
                 var enlaceSabrina = document.querySelector(".enlace_sabrina");
-                enlaceSabrina.href = /*relativePrefix +*/ "pages/recetas_sabrina.html";
+                enlaceSabrina.href = /*relativePrefix +*/ "/pages/recetas_sabrina.html";
     
                 var fotoNati = document.querySelector(".foto_natalia");
-                fotoNati.src = /*relativePrefix +*/ "images/foto_natalia.jpeg";
+                fotoNati.src = /*relativePrefix +*/ "/images/foto_natalia.jpeg";
     
                 var enlaceNatalia = document.querySelector(".enlace_natalia");
-                enlaceNatalia.href = /*relativePrefix +*/ "pages/recetas_natalia.html";
+                enlaceNatalia.href = /*relativePrefix +*/ "/pages/recetas_natalia.html";
                 
                 var fotoPablo = document.querySelector(".foto_pablo");
-                fotoPablo.src = /*relativePrefix +*/ "images/foto_pablo.jpg";
+                fotoPablo.src = /*relativePrefix +*/ "/images/foto_pablo.jpg";
     
                 var enlacePablo = document.querySelector(".enlace_pablo");
-                enlacePablo.href = /*relativePrefix +*/ "pages/recetas_pablo.html";
+                enlacePablo.href = /*relativePrefix +*/ "/pages/recetas_pablo.html";
             });
     
         // Cargar el footer
