aaabd2575a650f34efd06a5dea702027d1591b8f
NicoButter <nicobutter@gmail.com>
2023-10-24 13:36:14 -0300

    pruebas de prefijo

--- js/main.js
+++ js/main.js
@@ -36,16 +36,16 @@ function cargarHeaderYFooter() {
 
             // Actualizar las rutas de las imágenes en el header
             var logoDelicias = document.querySelector(".logoDelicias");
-            logoDelicias.src = relativePrefix + "codoacoco_23533__G8_el_recetario/images/logo_recetas_deliciosas.png";
+            logoDelicias.src = /*relativePrefix +*/ "codoacoco_23533__G8_el_recetario/images/logo_recetas_deliciosas.png";
 
             var enlaceLogoDelicias = document.querySelector(".enlace_logo_delicias");
-            enlaceLogoDelicias.href = relativePrefix + "codoacoco_23533__G8_el_recetario/index.html";
+            enlaceLogoDelicias.href = /*relativePrefix +*/ "codoacoco_23533__G8_el_recetario/index.html";
                       
             var fotoNicolas = document.querySelector(".foto_nicolas");
-            fotoNicolas.src = relativePrefix + "codoacoco_23533__G8_el_recetario/images/foto_nicolas.jpg";
+            fotoNicolas.src = /*relativePrefix +*/ "codoacoco_23533__G8_el_recetario/images/foto_nicolas.jpg";
 
             var enlaceNicolas = document.querySelector(".enlace_nicolas");
-            enlaceNicolas.href = relativePrefix + "codoacoco_23533__G8_el_recetario/pages/nicorecetas.html";
+            enlaceNicolas.href = /*relativePrefix +*/ "codoacoco_23533__G8_el_recetario/pages/nicorecetas.html";
 
             var fotoSabrina = document.querySelector(".foto_sabrina");
             fotoSabrina.src = relativePrefix + "codoacoco_23533__G8_el_recetario/images/foto_sabrina.jpg";
