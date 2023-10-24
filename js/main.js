bb22a2eb794c0eb9d0c50c70c35a8ff57b164d9f
NicoButter <nicobutter@gmail.com>
2023-10-24 12:29:52 -0300

    denuevo

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function shrinkHeader() {
 // Agrega un evento de desplazamiento (scroll) para llamar a la función shrinkHeader
 window.addEventListener("scroll", shrinkHeader);
 
-function cargarHeaderYFooter() {
+/*function cargarHeaderYFooter() {
     // Construir la ruta al archivo del header
     var rutaHeader = "pages/header.html";
     // Construir la ruta al archivo del footer
@@ -30,6 +30,78 @@ function cargarHeaderYFooter() {
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer").innerHTML = data;
+        });
+}*/
+
+function cargarHeaderYFooter() {
+    // Obtener la ruta absoluta actual
+    //var rutaAbsoluta = window.location.href;
+    var rutaAbsoluta = location.pathname;
+
+    // Calcular el nivel de subdirectorio actual
+    var nivelSubdirectorio = (rutaAbsoluta.match(/\//g) || []).length;
+
+    // Construir un prefijo relativo para las imágenes en base al nivel del subdirectorio
+    var relativePrefix = "";
+    for (var i = 0; i < nivelSubdirectorio; i++) {
+        relativePrefix += "../";
+    }
+
+    // Construir la ruta al archivo del header
+    var rutaHeader = "/pages/header.html";
+    // Construir la ruta al archivo del footer
+    var rutaFooter = "/pages/footer.html";
+    
+    // Cargar el header
+    fetch(rutaHeader)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
+
+            // Actualizar las rutas de las imágenes en el header
+            var logoDelicias = document.querySelector(".logoDelicias");
+            logoDelicias.src = relativePrefix + "/images/logo_recetas_deliciosas.png";
+
+            var enlaceLogoDelicias = document.querySelector(".enlace_logo_delicias");
+            enlaceLogoDelicias.href = relativePrefix + "/index.html";
+                      
+            var fotoNicolas = document.querySelector(".foto_nicolas");
+            fotoNicolas.src = relativePrefix + "/images/foto_nicolas.jpg";
+
+            var enlaceNicolas = document.querySelector(".enlace_nicolas");
+            enlaceNicolas.href = relativePrefix + "/pages/nicorecetas.html";
+
+            var fotoSabrina = document.querySelector(".foto_sabrina");
+            fotoSabrina.src = relativePrefix + "/images/foto_sabrina.jpg";
+
+            var enlaceSabrina = document.querySelector(".enlace_sabrina");
+            enlaceSabrina.href = relativePrefix + "/pages/recetas_sabrina.html";
+
+            var fotoNati = document.querySelector(".foto_natalia");
+            fotoNati.src = relativePrefix + "/images/foto_natalia.jpeg";
+
+            var enlaceNatalia = document.querySelector(".enlace_natalia");
+            enlaceNatalia.href = relativePrefix + "/pages/recetas_natalia.html";
+            
+            var fotoPablo = document.querySelector(".foto_pablo");
+            fotoPablo.src = relativePrefix + "/images/foto_pablo.jpg";
+
+            var enlacePablo = document.querySelector(".enlace_pablo");
+            enlacePablo.href = relativePrefix + "/pages/recetas_pablo.html";
+        });
+
+    // Cargar el footer
+    fetch(rutaFooter)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+
+            // Actualizar las rutas de las imágenes en el footer
+            var logoGrupo = document.querySelector(".logo-grupo");
+            logoGrupo.src = relativePrefix + "/images/logo_grupo_8.png";
+
+            var logoCodoACodo = document.querySelector(".logo-codo-a-codo");
+            logoCodoACodo.src = relativePrefix + "/images/logo_codo_a_codo.png";
         });
 }
 
