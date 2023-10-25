da6db03583e4f4a580ddfca2f2e97bfaa4c1e44b
NicoButter <nicobutter@gmail.com>
2023-10-24 21:18:44 -0300

    correcciones en el header y footer para las rutas relativas

--- js/main.js
+++ js/main.js
@@ -1,81 +1,62 @@
-// Código específico para el entorno de desarrollo
-    console.log('Estás en el entorno de desarrollo');
-
-    function cargarHeaderYFooter() {
-        // Obtener la ruta absoluta actual
-        //var rutaAbsoluta = window.location.href;
-        var rutaAbsoluta = location.pathname;
-    
-        // Calcular el nivel de subdirectorio actual
-        var nivelSubdirectorio = (rutaAbsoluta.match(/\//g) || []).length;
-    
-        // Construir un prefijo relativo para las imágenes en base al nivel del subdirectorio
-        var relativePrefix = "";
-        for (var i = 0; i < nivelSubdirectorio; i++) {
-            relativePrefix += "../";
-        }
-    
-        // Construir la ruta al archivo del header
-        var rutaHeader = "/pages/header.html";
-        // Construir la ruta al archivo del footer
-        var rutaFooter = "/pages/footer.html";
+// Construir la ruta al archivo del header
+var rutaHeader = "/pages/header.html";
+// Construir la ruta al archivo del footer
+var rutaFooter = "/pages/footer.html";
         
-        // Cargar el header
-        fetch(rutaHeader)
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById("header").innerHTML = data;
+    // Cargar el header
+    fetch(rutaHeader)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
     
-                // Actualizar las rutas de las imágenes en el header
-                var logoDelicias = document.querySelector(".logoDelicias");
-                logoDelicias.src = relativePrefix + "/images/logo_recetas_deliciosas.png";
+            // Actualizar las rutas de las imágenes en el header
+            var logoDelicias = document.querySelector(".logoDelicias");
+            logoDelicias.src =  "/images/logo_recetas_deliciosas.png";
     
-                var enlaceLogoDelicias = document.querySelector(".enlace_logo_delicias");
-                enlaceLogoDelicias.href = relativePrefix + "/index.html";
+            var enlaceLogoDelicias = document.querySelector(".enlace_logo_delicias");
+            enlaceLogoDelicias.href = "/index.html";
                           
-                var fotoNicolas = document.querySelector(".foto_nicolas");
-                fotoNicolas.src = relativePrefix + "/images/foto_nicolas.jpg";
+            var fotoNicolas = document.querySelector(".foto_nicolas");
+            fotoNicolas.src = "/images/foto_nicolas.jpg";
     
-                var enlaceNicolas = document.querySelector(".enlace_nicolas");
-                enlaceNicolas.href = relativePrefix + "/pages/nicorecetas.html";
+            var enlaceNicolas = document.querySelector(".enlace_nicolas");
+            enlaceNicolas.href = "/pages/nicorecetas.html";
     
-                var fotoSabrina = document.querySelector(".foto_sabrina");
-                fotoSabrina.src = relativePrefix + "/images/foto_sabrina.jpg";
+            var fotoSabrina = document.querySelector(".foto_sabrina");
+            fotoSabrina.src = "/images/foto_sabrina.jpg";
     
-                var enlaceSabrina = document.querySelector(".enlace_sabrina");
-                enlaceSabrina.href = relativePrefix + "/pages/recetas_sabrina.html";
+            var enlaceSabrina = document.querySelector(".enlace_sabrina");
+            enlaceSabrina.href = "/pages/recetas_sabrina.html";
     
-                var fotoNati = document.querySelector(".foto_natalia");
-                fotoNati.src = relativePrefix + "/images/foto_natalia.jpeg";
+            var fotoNati = document.querySelector(".foto_natalia");
+            fotoNati.src = "/images/foto_natalia.jpeg";
     
-                var enlaceNatalia = document.querySelector(".enlace_natalia");
-                enlaceNatalia.href = relativePrefix + "/pages/recetas_natalia.html";
+            var enlaceNatalia = document.querySelector(".enlace_natalia");
+            enlaceNatalia.href = "/pages/recetas_natalia.html";
                 
-                var fotoPablo = document.querySelector(".foto_pablo");
-                fotoPablo.src = relativePrefix + "/images/foto_pablo.jpg";
+            var fotoPablo = document.querySelector(".foto_pablo");
+            fotoPablo.src = "/images/foto_pablo.jpg";
     
-                var enlacePablo = document.querySelector(".enlace_pablo");
-                enlacePablo.href = relativePrefix + "/pages/recetas_pablo.html";
-            });
+            var enlacePablo = document.querySelector(".enlace_pablo");
+            enlacePablo.href = "/pages/recetas_pablo.html";
+    });
     
-        // Cargar el footer
-        fetch(rutaFooter)
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById("footer").innerHTML = data;
+    // Cargar el footer
+    fetch(rutaFooter)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
     
-                // Actualizar las rutas de las imágenes en el footer
-                var logoGrupo = document.querySelector(".logo-grupo");
-                logoGrupo.src = relativePrefix + "/images/logo_grupo_8.png";
+            // Actualizar las rutas de las imágenes en el footer
+            var logoGrupo = document.querySelector(".logo-grupo");
+            logoGrupo.src = "/images/logo_grupo_8.png";
     
-                var logoCodoACodo = document.querySelector(".logo-codo-a-codo");
-                logoCodoACodo.src = relativePrefix + "/images/logo_codo_a_codo.png";
-            });
-    }
+            var logoCodoACodo = document.querySelector(".logo-codo-a-codo");
+            logoCodoACodo.src = "/images/logo_codo_a_codo.png";
+    });
+
     
     cargarHeaderYFooter();
-
-
 
 var header = document.querySelector(".header");
 
